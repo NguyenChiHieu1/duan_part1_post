1a38f9627544d268dd6b5fad8483fecb99883181
Nguyen Chi Hieu <chihieunc99@gmail.com>
2024-02-27 11:23:07 +0700

    add edit delete

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers.js');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
@@ -13,6 +18,17 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUnitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URL
+    }),
+}))
 
 app.use(express.static('public'));
 
@@ -20,6 +36,8 @@ app.use(express.static('public'));
 app.use(expressLayout);
 app.set('layout', './layouts/main.ejs');
 app.set('view engine', 'ejs');
+
+app.locals.isActiveRoute = isActiveRoute;
 
 app.use('/', require('./server/routes/main'));
 app.use('/', require('./server/routes/admin'));
--- server/routes/main.js
+++ server/routes/main.js
@@ -31,7 +31,8 @@ router.get('', async (req, res) => {
             locals,
             data,
             current: page,
-            nextPage: hasNextPage ? nextPage : null
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute: '/'
         });
     } catch (error) {
         console.log(error);
@@ -53,7 +54,8 @@ router.get('/post/:id', async (req, res) => {
 
         const locals = {
             title: data.title,
-            description: " Simple Blog created with NodeJS, Express & MongoDb."
+            description: " Simple Blog created with NodeJS, Express & MongoDb.",
+            currentRoute: `/post/${slug}`
         }
 
         res.render('post', { locals, data });
@@ -101,7 +103,11 @@ router.post('/search', async (req, res) => {
 
 
 router.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',
+        {
+            currentRoute: '/about'
+        }
+    );
 });
 
 // function insertPostData() {
